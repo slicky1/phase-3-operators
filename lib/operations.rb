b63a1e41f270ed8713c60425fa679e22c849a268
slicky1 <okunonke@gmail.com>
2021-07-05 14:21:32 -0400

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
+    if speed < 40 || speed > 60
+      verdict = true
+    else
+      verdict = false
+    end
+    verdict
+  end
+  
+  
+  
+  def not_safe?(speed)
+    speed < 40 || speed > 60 ? true : false	
+  end
+      
+  
 
